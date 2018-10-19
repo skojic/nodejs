16bdcb6e902d645dc062008eda583ba75c9efcb6
Srdjan Kojic <srdjan.m.kojic@gmail.com>
2018-10-19 11:24:01 +0200

    refactoring code

--- express-demo/index.js
+++ express-demo/index.js
@@ -44,10 +44,7 @@ app.get('/api/courses', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
   const {error} = validateCourse(req.body);  
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -62,12 +59,10 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was not found.')
+  if (!course) return res.status(404).send('The course with the given ID was not found.')
+
   const {error} = validateCourse(req.body);  
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
   course.name = req.body.name;
   res.send(course);
 });
@@ -90,7 +85,19 @@ function validateCourse(course) {
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was not found.')
+  if (!course) return res.status(404).send('The course with the given ID was not found.')
+  res.send(course);
+});
+
+/**
+ * Creating DELETE route
+ */
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found.')
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
   res.send(course);
 });
 
@@ -102,5 +109,7 @@ app.get('/api/courses/:id', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
 
 
